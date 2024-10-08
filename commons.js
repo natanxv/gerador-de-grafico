7e80b542798371fb3c3b428c20f40bd24108898b
natanxv <114235253+natanxv@users.noreply.github.com>
2024-10-08 11:03:16 -0300

    Create commons.js

---
+++ commons.js
@@ -0,0 +1,11 @@
+constgetCSS=(variavel) =>{
+    const bodyStyles= getComputedStyle(document.body)
+    return bodyStyles.getPropertyValue(variavel)
+}
+
+const tickConfig={
+    family: getCSS('--font'),
+    size:16,
+    color:getCSS('--primary-color')
+}
+export{getCSS, tickConfig}
